60811c39835f5475a8b2a5b0bdb308a8a6d07f78
Osvaldo Lamas <osvaldolamas@gmail.com>
2016-08-30 20:03:34 -0300

    Adding initial client

--- crp-response-detector/src/main/java/com/qsocialnow/responsedetector/sources/TwitterClient.java
+++ crp-response-detector/src/main/java/com/qsocialnow/responsedetector/sources/TwitterClient.java
@@ -1,9 +1,28 @@
 package com.qsocialnow.responsedetector.sources;
 
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterClient {
+
+	private TwitterStream twitterStream;
 	
+	private ConfigurationBuilder configurationBuilder;
 	
+	public TwitterClient(){
+		configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.setOAuthConsumerKey("3urjVF7PN4bVATDa2hndrhfOI")
+                .setOAuthConsumerSecret("mpVGF9xtwY6TXDsBTn9bFLpIKoyXgwbMj9Ym7x7aq4dgdNOLdl")
+                .setOAuthAccessToken("768149646548041729-8UUVuboZZxpHctDZFtCDuz9lTdIYjXO")
+                .setOAuthAccessTokenSecret("2DqixWkUMYBTiImApgsHofUjUmabZHhiEKzXFiJf8CRTG");
+	}
+	
+	public void initClient(){
+
+		twitterStream = new TwitterStreamFactory(configurationBuilder.build()).getInstance();
+		
+	}
 	
 	
 }
