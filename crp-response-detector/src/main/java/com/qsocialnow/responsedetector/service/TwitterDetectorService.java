6be1b415c2aaa11dbe3944d52e07772a19a4c198
Osvaldo Lamas <osvaldolamas@gmail.com>
2016-09-23 16:25:23 -0300

    Improving case settings

--- crp-response-detector/src/main/java/com/qsocialnow/responsedetector/service/TwitterDetectorService.java
+++ crp-response-detector/src/main/java/com/qsocialnow/responsedetector/service/TwitterDetectorService.java
@@ -165,7 +165,7 @@ public class TwitterDetectorService extends SourceDetectorService {
             Long[] subSeries = { 9L };
             event.setSubSeriesId(subSeries);
 
-            event.setId("non");
+            event.setId(String.valueOf(status.getId()));
             event.setIdOriginal("odio");
             event.setTipoDeMedio("morbi");
             event.setConnotacion((short) 36);
@@ -190,7 +190,7 @@ public class TwitterDetectorService extends SourceDetectorService {
             event.setVersionTemas(new Date());
             event.setReproduccionesCount(Long.valueOf(status.getRetweetCount()));
             event.setLikeCount(0L);
-            event.setTitulo("Response detected from twitter source");
+            event.setTitulo(status.getText());
             event.setTexto(status.getText());
             event.setNormalizeMessage(status.getText());
             event.setUrlNoticia("");
