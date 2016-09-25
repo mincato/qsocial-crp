f007a9f16d986094412c85934e0baa13ec515e1e
elfrasco <adrianparedes82@gmail.com>
2016-09-25 16:42:20 -0300

    Agregado el nombre del branch en el home del manager web

--- crp-ticket-manager-web/src/main/java/com/qsocialnow/model/ApplicationVersion.java
+++ crp-ticket-manager-web/src/main/java/com/qsocialnow/model/ApplicationVersion.java
@@ -2,20 +2,27 @@ package com.qsocialnow.model;
 
 public class ApplicationVersion {
 
-    private final String version;
+	private final String version;
 
-    private final String build;
+	private final String build;
 
-    public ApplicationVersion(String version, String build) {
-        this.version = version;
-        this.build = build;
-    }
+	private final String branch;
 
-    public String getVersion() {
-        return version;
-    }
+	public ApplicationVersion(String version, String build, String branch) {
+		this.version = version;
+		this.build = build;
+		this.branch = branch;
+	}
 
-    public String getBuild() {
-        return build;
-    }
+	public String getVersion() {
+		return version;
+	}
+
+	public String getBuild() {
+		return build;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
 }
