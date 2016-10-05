4956affd7849096c44e98d35b36f58ae124487e7
Diego Saez <diego.saez@centauri.co>
2016-10-05 09:47:26 -0300

    Agregando logica de merge para los eventos que vienen del analitico

--- crp-event-resolver/src/main/java/com/qsocialnow/eventresolver/processor/MessageProcessor.java
+++ crp-event-resolver/src/main/java/com/qsocialnow/eventresolver/processor/MessageProcessor.java
@@ -1,15 +1,9 @@
 package com.qsocialnow.eventresolver.processor;
 
-import com.qsocialnow.eventresolver.service.DomainService;
 import com.qsocialnow.kafka.model.Message;
 
 public interface MessageProcessor {
 
     public void process(Message message) throws Exception;
 
-    public void setDomainElasticService(DomainService domainElasticService);
-
-    public void setDetectionMessageProcessor(DetectionMessageProcessor detectionMessageProcessor);
-
-    public void setExecutionMessageProcessor(ExecutionMessageProcessor executionMessageProcessor);
 }
