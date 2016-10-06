1be97970f161bc3b643b2e2c5646a08cd5c1ca48
paola.grajeda <paola.grajeda@gmail.com>
2016-10-06 17:01:21 -0300

    PRC-226: Validación de categorías al agregar criterio de detecc.

--- crp-ticket-manager-web/src/main/java/com/qsocialnow/validators/CriteriaFilterFormValidator.java
+++ crp-ticket-manager-web/src/main/java/com/qsocialnow/validators/CriteriaFilterFormValidator.java
@@ -11,6 +11,7 @@ import org.zkoss.bind.validator.AbstractValidator;
 import org.zkoss.util.resource.Labels;
 
 import com.qsocialnow.common.model.config.WordFilter;
+import com.qsocialnow.model.CategoryFilterView;
 import com.qsocialnow.model.FilterView;
 
 public class CriteriaFilterFormValidator extends AbstractValidator {
@@ -18,6 +19,8 @@ public class CriteriaFilterFormValidator extends AbstractValidator {
 	private static final String FILTERWORD_TEXT_FIELD_ID_PATTERN = "filterword_text_{0}";
 
 	private static final String FILTERWORD_TYPE_FIELD_ID_PATTERN = "filterword_type_{0}";
+
+	private static final String FILTERCATEGORY_FIELD_ID_PATTERN = "filtercategory_{0}";
 
 	private static final String FIELD_EMPTY_LABEL_KEY = "app.field.empty.validation";
 
@@ -37,6 +40,17 @@ public class CriteriaFilterFormValidator extends AbstractValidator {
 				if (wordFilter.getType() == null) {
 					addInvalidMessage(context, MessageFormat.format(
 							FILTERWORD_TYPE_FIELD_ID_PATTERN, i),
+							Labels.getLabel(FIELD_EMPTY_LABEL_KEY));
+				}
+
+			}
+		}
+		if (!CollectionUtils.isEmpty(filter.getFilterCategories())) {
+			for (int i = 0; i < filter.getFilterCategories().size(); i++) {
+				CategoryFilterView category = filter.getFilterCategories().get(i);
+				if (category.getCategoryGroup() == null) {
+					addInvalidMessage(context, MessageFormat.format(
+							FILTERCATEGORY_FIELD_ID_PATTERN, i),
 							Labels.getLabel(FIELD_EMPTY_LABEL_KEY));
 				}
 
