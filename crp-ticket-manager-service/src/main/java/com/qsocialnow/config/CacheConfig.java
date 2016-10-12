549bf9073f48916a66fc5b77ccfaa83501c44100
Diego Saez <diego.saez@centauri.co>
2016-10-12 16:41:39 -0300

    Agregando cache a los servicios externos

---
+++ crp-ticket-manager-service/src/main/java/com/qsocialnow/config/CacheConfig.java
@@ -0,0 +1,11 @@
+package com.qsocialnow.config;
+
+public interface CacheConfig {
+
+    public static final String THEMATICS_CACHE = "thematics";
+
+    public static final String CATEGORIES_CACHE = "categories";
+
+    public static final String USERS_CACHE = "users";
+
+}
