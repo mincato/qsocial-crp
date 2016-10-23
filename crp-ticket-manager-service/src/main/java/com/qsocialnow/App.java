142c3695440ec75ea17021a2c94beac3659adc7f
elfrasco <adrianparedes82@gmail.com>
2016-10-23 19:25:09 -0300

    Sobrescribiendo la configuración de log4j del service para que tome el log4j.xml

--- crp-ticket-manager-service/src/main/java/com/qsocialnow/App.java
+++ crp-ticket-manager-service/src/main/java/com/qsocialnow/App.java
@@ -1,36 +1,44 @@
 package com.qsocialnow;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.xml.DOMConfigurator;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.env.AbstractEnvironment;
 
 public class App {
 
-    private static final String DEFAULT_ENVIRONMENT = "development";
+	private static final String DEFAULT_ENVIRONMENT = "development";
 
-    private final ConfigurableApplicationContext context;
+	private final ConfigurableApplicationContext context;
 
-    private static App app;
+	private static App app;
 
-    public static App getInstance() {
-        if (app == null) {
-            app = new App();
-        }
-        return app;
-    }
+	public static App getInstance() {
+		if (app == null) {
+			app = new App();
+		}
+		return app;
+	}
 
-    private App() {
-        String env = System.getenv("environment");
-        if (env == null) {
-            env = DEFAULT_ENVIRONMENT;
-        }
-        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, env);
-        context = new ClassPathXmlApplicationContext("classpath:spring/applicationContext.xml");
-        context.registerShutdownHook();
-    }
+	private App() {
+		String env = System.getenv("environment");
+		if (env == null) {
+			env = DEFAULT_ENVIRONMENT;
+		}
+		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, env);
+		context = new ClassPathXmlApplicationContext("classpath:spring/applicationContext.xml");
+		context.registerShutdownHook();
+		configureLog4j();
+	}
 
-    public Object getBean(String bean) {
-        return context.getBean(bean);
-    }
+	public Object getBean(String bean) {
+		return context.getBean(bean);
+	}
+
+	private void configureLog4j() {
+		LogManager.resetConfiguration();
+		DOMConfigurator.configure("target/classes/log4j.xml");
+	}
 
 }
