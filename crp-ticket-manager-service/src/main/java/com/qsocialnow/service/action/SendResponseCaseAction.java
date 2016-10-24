4dbc0c68aa8a7588567dd60c8a4da46c88265adf
elfrasco <adrianparedes82@gmail.com>
2016-10-24 13:07:43 -0300

    Ya casi listo el deepLink para los replies

--- crp-ticket-manager-service/src/main/java/com/qsocialnow/service/action/SendResponseCaseAction.java
+++ crp-ticket-manager-service/src/main/java/com/qsocialnow/service/action/SendResponseCaseAction.java
@@ -27,83 +27,83 @@ import com.qsocialnow.persistence.UserResolverRepository;
 @Component("sendResponseCaseAction")
 public class SendResponseCaseAction implements Action {
 
-    private Logger log = LoggerFactory.getLogger(SendResponseCaseAction.class);
+	private Logger log = LoggerFactory.getLogger(SendResponseCaseAction.class);
 
-    @Resource
-    private Map<Long, SourceStrategy> sources;
+	@Resource
+	private Map<Long, SourceStrategy> sources;
 
-    @Autowired
-    private UserResolverRepository userResolverRepository;
+	@Autowired
+	private UserResolverRepository userResolverRepository;
 
-    @Autowired
-    private CuratorFramework zookeeperClient;
+	@Autowired
+	private CuratorFramework zookeeperClient;
 
-    @Value("${app.twitter.users.path}")
-    private String twitterUsersZnodePath;
+	@Value("${app.twitter.users.path}")
+	private String twitterUsersZnodePath;
 
-    @Value("${app.client}")
-    private String appClient;
+	@Value("${app.client}")
+	private String appClient;
 
-    @Value("${app.facebook.users.path}")
-    private String facebookUsersZnodePath;
+	@Value("${app.facebook.users.path}")
+	private String facebookUsersZnodePath;
 
-    @Override
-    public void execute(Case caseObject, Map<ActionParameter, Object> parameters) {
-        String text = (String) parameters.get(ActionParameter.TEXT);
-        String userResolverId;
-        if (caseObject.getUserResolver() == null) {
-            userResolverId = (String) parameters.get(ActionParameter.USER_RESOLVER);
-        } else {
-            userResolverId = caseObject.getUserResolver().getId();
-        }
-        UserResolver userResolver = userResolverRepository.findOne(userResolverId);
-        String lastPostId = caseObject.getLastPostId();
-        String postId = sources.get(userResolver.getSource()).sendResponse(caseObject, userResolver, text);
+	@Override
+	public void execute(Case caseObject, Map<ActionParameter, Object> parameters) {
+		String text = (String) parameters.get(ActionParameter.TEXT);
+		String userResolverId;
+		if (caseObject.getUserResolver() == null) {
+			userResolverId = (String) parameters.get(ActionParameter.USER_RESOLVER);
+		} else {
+			userResolverId = caseObject.getUserResolver().getId();
+		}
+		UserResolver userResolver = userResolverRepository.findOne(userResolverId);
+		String lastPostId = caseObject.getLastPostId();
+		String postId = sources.get(userResolver.getSource()).sendResponse(caseObject, userResolver, text);
 
-        if (caseObject.getIdRootComment() == null)
-            caseObject.setIdRootComment(postId);
+		if (caseObject.getIdRootComment() == null)
+			caseObject.setIdRootComment(postId);
 
-        caseObject.setPendingResponse(false);
-        caseObject.setLastPostId(postId);
-        caseObject.setUserResolver(new BaseUserResolver(userResolver));
-        caseObject.addMessage(new Message(postId, false));
-        parameters.put(ActionParameter.COMMENT, text);
+		caseObject.setPendingResponse(false);
+		caseObject.setLastPostId(postId);
+		caseObject.setUserResolver(new BaseUserResolver(userResolver));
+		caseObject.addMessage(new Message(postId, false));
+		parameters.put(ActionParameter.COMMENT, text);
 
-        createNewUserResolverUserNode(userResolver, caseObject, lastPostId, postId);
-    }
+		createNewUserResolverUserNode(userResolver, caseObject, lastPostId, postId);
+	}
 
-    private void createNewUserResolverUserNode(UserResolver userResolver, Case caseObject, String lastPostId,
-            String postId) {
-        try {
-            if (FilterConstants.MEDIA_TWITTER.equals(userResolver.getSource())) {
-                String clientTwitterUsersZnodePath = MessageFormat.format(twitterUsersZnodePath, appClient);
-                TwitterMessageEvent messageEvent = new TwitterMessageEvent(caseObject.getId(), lastPostId, caseObject
-                        .getSubject().getSourceId(), postId, lastPostId);
+	private void createNewUserResolverUserNode(UserResolver userResolver, Case caseObject, String lastPostId,
+			String postId) {
+		try {
+			if (FilterConstants.MEDIA_TWITTER.equals(userResolver.getSource())) {
+				String clientTwitterUsersZnodePath = MessageFormat.format(twitterUsersZnodePath, appClient);
+				TwitterMessageEvent messageEvent = new TwitterMessageEvent(caseObject.getId(), lastPostId,
+						caseObject.getSubject().getSourceId(), postId, lastPostId);
 
-                String twitterEvent = new Gson().toJson(messageEvent);
-                log.info("Adding a twitter user converstation:" + twitterEvent);
-                zookeeperClient.create().forPath(
-                        clientTwitterUsersZnodePath + "/" + userResolver.getIdentifier() + "/" + postId,
-                        twitterEvent.getBytes());
-            }
+				String twitterEvent = new Gson().toJson(messageEvent);
+				log.info("Adding a twitter user converstation:" + twitterEvent);
+				zookeeperClient.create().forPath(
+						clientTwitterUsersZnodePath + "/" + userResolver.getIdentifier() + "/" + postId,
+						twitterEvent.getBytes());
+			}
 
-            if (FilterConstants.MEDIA_FACEBOOK.equals(userResolver.getSource())) {
-                String clientFacebookUsersZnodePath = MessageFormat.format(facebookUsersZnodePath, appClient);
-                FacebookFeedEvent facebookFeedEvent = new FacebookFeedEvent(caseObject.getId(),
-                        caseObject.getLastPostId(), caseObject.getSubject().getSourceId(), caseObject.getSubject()
-                                .getIdentifier(), postId, lastPostId, postId, caseObject.getIdRootComment(),
-                        userResolver.getIdentifier());
+			if (FilterConstants.MEDIA_FACEBOOK.equals(userResolver.getSource())) {
+				String clientFacebookUsersZnodePath = MessageFormat.format(facebookUsersZnodePath, appClient);
+				FacebookFeedEvent facebookFeedEvent = new FacebookFeedEvent(caseObject.getId(),
+						caseObject.getLastPostId(), caseObject.getSubject().getSourceId(),
+						caseObject.getSubject().getIdentifier(), postId, lastPostId, postId,
+						caseObject.getIdRootComment(), userResolver.getIdentifier(),
+						caseObject.getTriggerEvent().getIdOriginal());
 
-                String facebookEvent = new Gson().toJson(facebookFeedEvent);
+				String facebookEvent = new Gson().toJson(facebookFeedEvent);
 
-                log.info("Adding a facebook user converstation:" + facebookEvent);
-                zookeeperClient.create().forPath(
-                        clientFacebookUsersZnodePath + "/" + userResolver.getIdentifier() + "/"
-                                + caseObject.getIdRootComment(), facebookEvent.getBytes());
-            }
-        } catch (Exception e) {
-            log.error("Unexpected error trying to creade conversation node to be consumed by response", e);
-        }
-    }
+				log.info("Adding a facebook user converstation:" + facebookEvent);
+				zookeeperClient.create().forPath(clientFacebookUsersZnodePath + "/" + userResolver.getIdentifier() + "/"
+						+ caseObject.getIdRootComment(), facebookEvent.getBytes());
+			}
+		} catch (Exception e) {
+			log.error("Unexpected error trying to creade conversation node to be consumed by response", e);
+		}
+	}
 
 }
