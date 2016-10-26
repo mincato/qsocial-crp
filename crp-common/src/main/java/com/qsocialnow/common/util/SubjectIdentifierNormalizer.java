e0ce93213dc723ca41149486d6291966c85ecfdc
Diego Saez <diego.saez@centauri.co>
2016-10-26 09:51:18 -0300

    formateando archivos

--- crp-common/src/main/java/com/qsocialnow/common/util/SubjectIdentifierNormalizer.java
+++ crp-common/src/main/java/com/qsocialnow/common/util/SubjectIdentifierNormalizer.java
@@ -4,13 +4,13 @@ import org.apache.commons.lang3.StringUtils;
 
 public class SubjectIdentifierNormalizer {
 
-	public static String normalize(String identifier) {
-		if (StringUtils.isNotBlank(identifier)) {
-			String[] identifierTokens = identifier.split("\\s+http");
-			if (identifierTokens.length > 0) {
-				return identifierTokens[0];
-			}
-		}
-		return identifier;
-	}
+    public static String normalize(String identifier) {
+        if (StringUtils.isNotBlank(identifier)) {
+            String[] identifierTokens = identifier.split("\\s+http");
+            if (identifierTokens.length > 0) {
+                return identifierTokens[0];
+            }
+        }
+        return identifier;
+    }
 }
