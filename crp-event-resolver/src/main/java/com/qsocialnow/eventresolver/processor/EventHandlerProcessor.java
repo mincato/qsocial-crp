484c4fc630e3e11fb3807f1be81eb8b2c668ea1d
elfrasco <adrianparedes82@gmail.com>
2016-11-10 16:46:01 -0300

    Agregado log especial para loguear eventos

--- crp-event-resolver/src/main/java/com/qsocialnow/eventresolver/processor/EventHandlerProcessor.java
+++ crp-event-resolver/src/main/java/com/qsocialnow/eventresolver/processor/EventHandlerProcessor.java
@@ -7,14 +7,17 @@ import com.qsocialnow.common.exception.RepositoryException;
 import com.qsocialnow.common.queues.QueueConsumer;
 import com.qsocialnow.common.queues.QueueProducer;
 import com.qsocialnow.common.queues.QueueService;
-import com.qsocialnow.common.queues.QueueType;
+import com.qsocialnow.eventresolver.App;
+import com.qsocialnow.eventresolver.Consumers;
 import com.qsocialnow.eventresolver.exception.InvalidDomainException;
 import com.qsocialnow.kafka.consumer.Consumer;
 import com.qsocialnow.kafka.model.Message;
 
 public class EventHandlerProcessor implements Runnable {
 
-    private static final Logger log = LoggerFactory.getLogger(EventHandlerProcessor.class);
+    private static final Logger ROOT_LOGGER = LoggerFactory.getLogger(EventHandlerProcessor.class);
+
+    private static final Logger EVENT_LOGGER = LoggerFactory.getLogger("eventLogger");
 
     private final Consumer consumer;
 
@@ -37,16 +40,17 @@ public class EventHandlerProcessor implements Runnable {
         initQueueService();
         Message message = consumer.consume();
         while (message != null) {
+            logEvent(message);
             try {
                 messageProcessor.process(message);
             } catch (InvalidDomainException invalidDomainExecption) {
-                log.error("There was an error processing event", invalidDomainExecption);
+                ROOT_LOGGER.error("There was an error processing event", invalidDomainExecption);
                 addFailEvent(message);
             } catch (RepositoryException sourceException) {
-                log.error("There was an error processing event", sourceException);
+                ROOT_LOGGER.error("There was an error processing event", sourceException);
                 addFailEvent(message);
             } catch (Exception e) {
-                log.error("Unexpected error: ", e);
+                ROOT_LOGGER.error("Unexpected error: ", e);
                 addDeadEvent(message);
             }
             message = consumer.consume();
@@ -77,5 +81,14 @@ public class EventHandlerProcessor implements Runnable {
         queueProducer.stopProducer();
         queueConsumer.stopConsumer();
         queueService.shutdownQueueService();
+    }
+
+    private void logEvent(Message message) {
+        try {
+            if (message.getGroup().equals(Consumers.RESPONSE_DETECTOR_CONSUMER_NAME)) {
+                EVENT_LOGGER.info(message.getMessage());
+            }
+        } catch (Exception e) {
+        }
     }
 }
