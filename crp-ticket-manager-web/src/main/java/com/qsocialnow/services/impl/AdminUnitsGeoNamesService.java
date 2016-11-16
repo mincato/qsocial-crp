724ea7619f8676c011485ef8ceb70e678767e3a3
Osvaldo Lamas <osvaldolamas@gmail.com>
2016-11-16 15:50:50 -0300

    Adding translation service of unit admins

---
+++ crp-ticket-manager-web/src/main/java/com/qsocialnow/services/impl/AdminUnitsGeoNamesService.java
@@ -0,0 +1,69 @@
+package com.qsocialnow.services.impl;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.qsocialnow.common.model.config.AdminUnit;
+import com.qsocialnow.model.AdminUnitResponse;
+import com.qsocialnow.services.AdminUnitsService;
+
+@Service("adminUnitsService")
+@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class AdminUnitsGeoNamesService implements AdminUnitsService {
+
+    private static final Logger log = LoggerFactory.getLogger(AdminUnitsGeoNamesService.class);
+
+    @Autowired
+    private String adminUnitServiceUrl;
+
+    @Override
+    public List<AdminUnit> findUnitAdminsByGeoIds(String query, Object parameters) {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(adminUnitServiceUrl).queryParam(
+                    "geoNameIds", query);
+
+            if (parameters != null) {
+                String language = (String) parameters;
+                builder.queryParam("language", language);
+            }
+
+            RestTemplate restTemplate = new RestTemplate();
+            List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+            MappingJackson2HttpMessageConverter jsonMessageConverter = new MappingJackson2HttpMessageConverter();
+            jsonMessageConverter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON, MediaType.TEXT_HTML));
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            jsonMessageConverter.setObjectMapper(mapper);
+            messageConverters.add(jsonMessageConverter);
+            restTemplate.setMessageConverters(messageConverters);
+            AdminUnitResponse response = restTemplate.getForObject(builder.toUriString(), AdminUnitResponse.class);
+
+            return response.getAdministrativeUnits();
+        } catch (Exception e) {
+            log.error("There was an error while trying to call admin unit service", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+}
