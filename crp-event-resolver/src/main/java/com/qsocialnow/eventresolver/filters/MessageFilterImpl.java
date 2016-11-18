26eac67d38ab10f8978a67d2065b14b57315a308
elfrasco <adrianparedes82@gmail.com>
2016-11-18 14:41:47 -0300

    Haciendo una prueba

--- crp-event-resolver/src/main/java/com/qsocialnow/eventresolver/filters/MessageFilterImpl.java
+++ crp-event-resolver/src/main/java/com/qsocialnow/eventresolver/filters/MessageFilterImpl.java
@@ -10,12 +10,14 @@ import com.qsocialnow.common.model.event.Event;
 @Service
 @Qualifier("messageFilter")
 public class MessageFilterImpl implements MessageFilter {
-
+	
     @Override
     public boolean shouldProcess(Event message, Domain domain) {
         boolean shouldProcess = false;
 
         if (domain != null && CollectionUtils.isNotEmpty(domain.getThematics())) {
+        	System.out.println("=========> " + domain.getThematics());
+        	System.out.println("=========> " + message.getTokenId());
             shouldProcess = domain.getThematics().stream().anyMatch(thematic -> thematic.equals(message.getTokenId()));
         }
 
